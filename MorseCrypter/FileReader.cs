32ecb114c4cb49f457e531f6c6816a8e77fe2bff
Tom <125987923+TomsDex@users.noreply.github.com>
2024-03-26 15:51:06 +0000

    Started to implement filereader. Included files in commit

--- MorseCrypter/FileReader.cs
+++ MorseCrypter/FileReader.cs
@@ -1,9 +1,67 @@
-﻿using System.Threading.Channels;
-
-namespace MorseCrypter
+﻿namespace MorseCrypter
 {
-    internal class FileReader
+    /// <summary>
+    /// Provides a set of functions to read the translation sets from provided files.
+    /// </summary>
+    public class FileReader
     {
+        public string Directory { get; set; }
+        public string FileName { get; set; }
+        public string[] Text { get; set; }
+        public Dictionary<char, string> CharacterSet { get; set; }
+
+        public FileReader()
+        {
+            Directory = GetDirectory();
+            FileName = GetFileName();
+            Text = ReadFile(FileName);
+            CharacterSet = ReturnCharacterSet();
+        }
+
+        /// <summary>
+        /// Initialise directory of translation sets.
+        /// </summary>
+        /// <returns>The directory in which the translation sets are saved.</returns>
+        public string GetDirectory()
+        {
+            return "..\\..\\..\\..\\resources\\";
+        }
+
+        /// <summary>
+        /// Get the name of the file to be read.
+        /// </summary>
+        /// <returns>The file name.</returns>
+        public string GetFileName()
+        {
+            return "international.txt";
+        }
+
+        /// <summary>
+        /// Reads every line in the provided file.
+        /// </summary>
+        /// <param name="FileName">The name of the file to be read.</param>
+        /// <returns>All lines within the specified file.</returns>
+        public string[] ReadFile(string FileName)
+        {
+            return File.ReadAllLines(Directory + FileName);
+        }
+
+        public Dictionary<char, string> ReturnCharacterSet()
+        {
+            //TODO: Finish this
+
+            //Initialise a dictionary to store the morse code translation set.
+            Dictionary<char, string> MorseCodeTranslationSet = new Dictionary<char, string>();
+            return null;
+        }
         
+        public void WriteToConsole()
+        {
+            foreach (var s in Text)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
     }
 }
--- MorseCrypter/Program.cs
+++ MorseCrypter/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MorseCrypter;
+
+FileReader filereader = new FileReader();
+filereader.WriteToConsole();
