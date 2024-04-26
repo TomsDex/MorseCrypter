dc15e4996076dd058c3c43a8830af70e2648760b
Tom <TomsDex@proton.me>
2024-04-26 18:25:23 +0100

    Implemented compression

---
+++ MorseCrypter.Core/GZIP.cs
@@ -0,0 +1,32 @@
+﻿using System.IO.Compression;
+
+namespace MorseCrypter.Core
+{
+    public class GZIP
+    {
+        public static byte[] Compress(byte[] buffer)
+        {
+            using var memStream = new MemoryStream();
+
+            using (var gZipStream = new GZipStream(memStream, CompressionMode.Compress, true))
+            {
+                gZipStream.Write(buffer, 0, buffer.Length);
+            }
+
+            return memStream.ToArray();
+        }
+
+        public static byte[] Decompress(byte[] compressedData)
+        {
+            using var memStream = new MemoryStream(compressedData);
+
+            using var gZipStream = new GZipStream(memStream, CompressionMode.Decompress);
+
+            using var resultStream = new MemoryStream();
+
+            gZipStream.CopyTo(resultStream);
+
+            return resultStream.ToArray();
+        }
+    }
+}
