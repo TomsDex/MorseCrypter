958ba2ed2d9a4a73bcffde60b3b4863c78cbb770
Tom <TomsDex@proton.me>
2024-04-27 18:16:14 +0100

    Implemented hex encoder and renamed encryption

--- MorseCrypter.Core/Encoder.cs
+++ MorseCrypter.Core/Encoder.cs
@@ -1,6 +1,20 @@
-﻿namespace MorseCrypter.Core;
+﻿using System.Text;
 
-public class EncodeTextToBase36
+namespace MorseCrypter.Core
 {
+    public class Encoder
+    {
+        public string Encode(string text)
+        {
+            var bytes = Encoding.UTF8.GetBytes(text);
+            return Convert.ToHexString(bytes);
 
+        }
+
+        public string Decode(string hex)
+        {
+            var bytes = Convert.FromHexString(hex);
+            return Encoding.UTF8.GetString(bytes);
+        }
+    }
 }
--- MorseCrypter.Core/Encryption.cs
+++ MorseCrypter.Core/Encryption.cs
@@ -2,7 +2,7 @@ using System.Security.Cryptography;
 
 namespace MorseCrypter.Core;
 
-public static class Encrypter
+public static class Encryption
 {
     private const int KeyBytes = 32; // 256-bit key
     private const int SaltSize = 16; // 128-bit salt
